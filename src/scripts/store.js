446190e94b20e19c783d1c9e3aae00a6dae05e24
Jesse Smart <jessemillersmart@gmail.com>
2020-10-07 10:26:56 -0400

    store

---
+++ src/scripts/store.js
@@ -0,0 +1,34 @@
+const store = {
+  bookmarks: [],
+  adding: false,
+  error: null,
+  filter: 0,
+};
+
+const addItem = function (item) {
+  this.store.bookmarks.push(item);
+};
+
+const findById = function (id) {
+  return this.store.bookmarks.find((currentItem) => currentItem.id === id);
+};
+
+const findAndDelete = function (id) {
+  this.store.bookmarks = this.store.bookmarks.filter(
+    (currentItem) => currentItem.id !== id
+  );
+};
+
+function filterResults(val) {
+  this.store.filter = this.store.bookmarks.filter(
+    (currentItem) => currentItem.rating >= this.store.filter
+  );
+}
+
+export default {
+  store,
+  addItem,
+  findAndDelete,
+  findById,
+  filterResults,
+};
